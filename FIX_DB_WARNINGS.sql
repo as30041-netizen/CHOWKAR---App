ba78411ca86c8ff05c87f9131d5950ec4ef66b85
as30041-netizen <as30041@gmail.com>
2025-12-13 10:47:06 +0530

    Update FIX_DB_WARNINGS.sql

--- FIX_DB_WARNINGS.sql
+++ FIX_DB_WARNINGS.sql
@@ -1,12 +1,9 @@
--- FIX SECURITY WARNINGS (Mutable Search Path)
--- Explicitly force functions to use ONLY the 'public' schema
--- This prevents "search path hijacking" attacks.
+-- FIX SECURITY WARNINGS (Corrected)
+-- Only targeting functions we confirmed exist.
 
 ALTER FUNCTION public.accept_bid(uuid, uuid, uuid, uuid, integer, integer) SET search_path = public;
 
 ALTER FUNCTION public.update_user_rating() SET search_path = public;
 
--- Also fixing common helpers likely present in your DB
-ALTER FUNCTION public.calculate_distance(double precision, double precision, double precision, double precision) SET search_path = public;
-
-ALTER FUNCTION public.update_updated_at_column() SET search_path = public;
+-- Optional: try this, but if it fails, ignore it.
+-- ALTER FUNCTION public.update_updated_at_column() SET search_path = public;
